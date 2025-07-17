cfb170109e07b48d3e4a7d55f0f58e3cd90703de
sara <sara.atef001@gmail.com>
2025-07-17 11:44:31 +0300

    day03

---
+++ JS/Day03/demo.js
@@ -0,0 +1,142 @@
+/**=========================== */
+
+/**========== */
+
+// var trackName = "dotnet";
+// function greet() {
+// 	var trackName = "mearn";
+// 	console.log(trackName);
+// }
+// var result = greet();
+// console.log(trackName);
+// console.log(result);
+
+/**============================ */
+// var trackName = "dotnet";
+// function greet() {
+// 	console.log(trackName);
+// 	trackName = "mearn";
+// 	console.log(trackName);
+// }
+// var result = greet();
+// console.log(trackName);
+// console.log(result);
+/**=========================== */
+
+// function sayHello(trackName) {
+// 	console.log("Welcome", trackName + "at iti");
+// }
+
+// function greet(trackName) {
+// 	console.log("Hello New INTAKE");
+// 	return sayHello(trackName);
+// }
+
+// console.log(greet("dotnet"));
+
+// var fname1 = "ali";
+// var y = 25; //
+
+// var fname2 = "mona";
+
+// var x = 23;
+
+// console.log(age2); // mona
+
+// console.log(age1);
+
+/** object properties
+ *  == property [key]:value (any datatypes)
+ *  == methods
+ *
+ */
+
+// access
+// dot notation
+// subscript
+// user defined object
+// var person1 = {
+// 	fname: "ali",
+// 	age: 25,
+// 	address: {
+// 		city: "alex",
+// 		code: 123,
+// 		street: "mohamed ali Str",
+// 	},
+// 	// method [props][methods]
+// 	display: function (emoji) {
+// 		console.log(person1.fname + emoji);
+// 		//return { createdBy: "Sara", time: "12:00" };
+// 	},
+// 	colors: ["red", "green", "blue"],
+// };
+
+// var result = person1.display("^_^");
+
+// console.log(result);
+// console.log(person1.display()); // undefined
+// set
+// person1["fname"] = "alaa";
+// person1["gender"] = "male";
+
+// console.log(person1);
+//  set -- prop -override
+// person1.fname = "ahmed";
+
+// // [prop] ==> not found -- add
+// person1.gender = "male";
+// console.log(person1);
+// console.log(person1["fname"]);
+// console.log(person1["age"]);
+// console.log(person1["address"]["city"]);
+// console.log(person1.address["city"]);
+// console.log(person1["address"].city);
+
+// // var key = "fname";
+// console.log(person1["key"]);
+// get
+
+// var age = person1.age;
+// console.log(person1.fname);
+// console.log(person1.age);
+// console.log(person1.address.city);
+// // get
+// console.log(person1.gender);
+
+/// built int
+// var str = "hello at iti";
+
+// console.log(str.length); // prop
+
+// var result = str.toUpperCase();
+
+// console.log(Math.PI);
+
+/**
+ * PI
+ * -------------------
+ * 	== sqrt
+ * 	== pow
+ * 	== sign == > Positive == 1
+ * 	== sign == > negative == -1
+ * 	== sign == > 0 == 0
+ * 	== sin
+ * 	== cos
+ * 	== tas
+ * == round
+ * == max
+ * == min
+ */
+
+//
+// console.log(Math.max(10, 30, 60, 80, 30));
+// console.log(Math.min(10, 30, 60, 80, 30));
+// console.log(Math.floor(Math.random() * 10));
+// console.log(Math.floor(5.9));
+// console.log(Math.ceil(5.1));
+// console.log(Math.sin((90 * Math.PI) / 180));
+// console.log(Math.sign(20));
+// console.log(Math.sign(-10));
+// console.log(Math.sign(0));
+// console.log(Math.pow(2, 3));
+// console.log(Math.sqrt(25));
---
+++ JS/Day03/main.js
@@ -0,0 +1,66 @@
+// function sum(x, y) {
+// 	console.log("Sum Function");
+// 	return x + y;
+// }
+
+// var result = sum(3, 4);
+
+// console.log(result);
+
+/**======================= */
+
+function greet() {
+	var trackName = "mearn";
+	console.log("Welcome at ITI");
+	console.log(trackName);
+
+	return "ITI";
+}
+
+var result = greet();
+console.log(result);
+console.log("after greet execution");
+// function sum(x, y) {
+// 	console.log("Sum Function");
+// 	return x + y;
+// }
+
+// var result = sum(3, 4);
+
+// console.log(result);
+
+/**======================= */
+// var trackName = "mearn";
+// function greet() {
+// 	console.log(trackName);
+// 	console.log("Welcome at ITI");
+// 	return "ITI";
+// }
+
+// var result = greet();
+// console.log( trackName );
+// mearn - welcome at iti - mearn 14
+// mearn - welcome at iti - iti - mearn 1
+// mearn - welcome - Error 1
+//Error - Welcome - mearn 1
+// //
+// var trackName = "mearn";
+// function greet() {
+// 	trackName = "dotnet";
+// 	console.log(trackName);
+// 	return "ITI";
+// }
+
+// var result = greet();
+// console.log(trackName);
+
+/**=================== */
+
+function greet() {
+	console.log(trackName);
+	return "ITI";
+}
+
+var result = greet();
+console.log(trackName);
+console.log(result);
