39e61e559b84442f1aeb2b0cbe6aec2b518f6b15
sara <sara.atef001@gmail.com>
2025-07-20 13:30:40 +0300

    day04

---
+++ JS/Day04/demo.js
@@ -0,0 +1,45 @@
+//var p = document.getElementsByClassName("info")[0];
+
+// function changeStyle() {
+// 	// style
+// 	p.style.backgroundColor = "#7882a9";
+// 	p.style.padding = "1rem";
+// 	p.style.color = "#fff";
+// }
+// function changeStyle() {
+// 	p.className += " jsClass"; //override
+// 	// get
+// 	// console.log(p.className);
+// }
+// function changeStyle() {
+// 	p.classList.toggle("jsClass");
+// 	// p.classList.remove("testP");
+// 	// p.classList.add("jsClass");
+// 	// console.log(p.classList);
+// }
+
+// var link1 = document.getElementById("link1");
+// var p = document.getElementsByClassName("info")[0];
+// var img1 = document.images[0];
+
+// function execute() {
+// 	// p.setAttribute("class", "jsClass");
+// 	// if (link1.hasAttribute("href")) {
+// 	// 	link1.removeAttribute("href");
+// 	// } else {
+// 	// 	link1.setAttribute("href", "#google");
+// 	// }
+// 	// link1.setAttribute("href", "#twiiter");
+// 	// img1.setAttribute("src", "./2.jpg");
+// 	// console.log(link1.getAttribute("href"));
+// 	// console.log(img1.getAttribute("src"));
+// }
+// var trackName = "Mearn";
+
+// console.log(trackName);
+// for (var i = 0; i < 6; i++) {
+// 	console.log("hello ", i);
+// }
+
+// console.log("----------------------");
+// console.log("Welcome at iti");
---
+++ JS/Day04/main.js
@@ -0,0 +1,60 @@
+// dom --- Document object model
+
+// var div = document.querySelectorAll("#parent")[0];
+
+// console.log(div.children); //
+// console.log(div.childNodes);
+// console.log(div.firstChild);
+
+// console.log(div.firstElementChild);
+
+// console.log(document.images);
+// console.log(document.links[0]);
+// console.log(document.links[1]);
+// console.log(document.links[2]);
+// var element = document.getElementById("h1");
+// var div = document.getElementById("parent");
+
+// var element = document.getElementsByClassName("info");
+// var elements = div.getElementsByClassName("info");
+
+// var elements = document.getElementsByTagName("div");
+// var elements = document.querySelector("p");
+
+// console.log(elements); //
+// console.log(elements[0]); //
+// console.log(elements);
+// for (var i = 0; i < elements.length; i++) {
+// 	console.log(elements[i]);
+// }
+// var h1 = document.querySelector("p");
+
+// var changeText = function () {
+//h1.innerText = "<span>ahmed ali</span>";
+// h1.innerHTML = "<span>Welcome at iti</span>";
+// console.log(h1.innerText);
+// console.log(h1.innerHTML);
+// h1.innerHTML = "<h1>Welcome at iti</h1>";
+// console.log(h1.innerHTML);
+// console.log(h1.innerText); // get
+// };
+
+// var input1 = document.getElementById("input1");
+// var output = document.getElementById("output");
+
+// function execute() {
+// 	output.innerHTML = input1.value;
+// 	// input1.value = "React";
+// 	// console.log(input1.value);//get
+// }
+
+// var inputs = document.getElementsByName("choose");
+// var output = document.getElementById("output");
+
+// function showAnswer() {
+// 	for (var i = 0; i < inputs.length; i++) {
+// 		if (inputs[i].checked) {
+// 			console.log(inputs[i].value);
+// 		}
+// 	}
+// }
